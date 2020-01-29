9e2eaa52e2f0a2d64d1a0a01ac69b04b7d468f81
gyounghan <hkh2796@naver.com>
2020-01-29 23:36:40 +0900

    장고 ORM 실습
    Person 데이터베이스 생성 (first_name, last_name, age, updateDate)

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField(null = True)
+    updateDate = models.DateTimeField(auto_now = True)
+    # Create your models here.
+    #
+    def __str__(self):
+        return '%s %s' %(self.first_name,self.last_name)
