9be0c530bea21dd687a35f1aa585e2df74c4b7c6
gyounghan <hkh2796@naver.com>
2020-01-30 23:54:52 +0900

    회원가입 실습
    회원가입.html를 통해 ID, Password를 데이터베이스에 저장

--- hello/models.py
+++ hello/models.py
@@ -5,8 +5,11 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(null = True)
-    updateDate = models.DateTimeField(auto_now = True)
     # Create your models here.
     #
     def __str__(self):
         return '%s %s' %(self.first_name,self.last_name)
+
+class User_Info(models.Model):
+    UserId = models.CharField(max_length = 30)
+    Password = models.CharField(max_length = 30)
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
 
 # Create your views here.
 def hello(request):
+    
     return render(request, 'home.html')
+
+def signup(request):
+    
+    if request.method == 'POST' :
+       
+        user_id = request.POST['username']
+
+        password = request.POST['password']
+
+
+        new_User = User_Info(UserId = user_id, Password = password)
+
+        new_User.save()
+
+    else :
+        
+        signup_form = UserCreationForm()
+    
+    return render(request, 'signup.html')
+
+
+    
