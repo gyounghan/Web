045cd1cd5081fbc552b31da564a007d12e86c7e1
gyounghan <hkh2796@naver.com>
2020-02-04 21:39:59 +0900

    회원가입 기능 개발
    회원가입 forms.py을 이용하여 회원가입 페이지

--- hello/urls.py
+++ hello/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello',views.hello),
+    path('hello',views.hello, name='home' ),
     path('signup',views.signup),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
+from django.contrib import messages
+from .forms import UserForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def hello(request):
@@ -9,22 +14,24 @@ def hello(request):
 
 def signup(request):
     
-    if request.method == 'POST' :
-       
-        user_id = request.POST['username']
-
-        password = request.POST['password']
-
-
-        new_User = User_Info(UserId = user_id, Password = password)
-
-        new_User.save()
-
-    else :
-        
-        signup_form = UserCreationForm()
+    if request.method == "POST":
     
-    return render(request, 'signup.html')
-
-
+        form = UserForm(request.POST)
     
+        if form.is_valid():
+    
+            new_user = User.objects.create_user(**form.cleaned_data)
+
+            login(request, new_user)
+
+            return redirect('home')
+    
+        else:
+    
+            return HttpResponse('사용자명이 이미 존재합니다.')
+    
+    else:
+    
+        form = UserForm()
+    
+        return render(request, 'signup.html', {'form': form})
