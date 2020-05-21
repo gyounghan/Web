3c024be9c1ee7778ec9866d162aba76f5e7f75b9
gyounghan <hkh2796@naver.com>
2020-05-21 20:10:44 +0900

    signup
    - id, password, email 작성하여 signup
    - 작성된 email 주소로 계정활성화 email 전송

---
+++ blank_meeting/account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('index/', views.index, name='index'),
+    path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'),
+]
