9825d6316b76d13ac493c29a4e649f8d670cc6d0
gyounghan <hkh2796@naver.com>
2020-07-09 22:54:51 +0900

    리뷰 게시물 등록

---
+++ blank_meeting/review/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Review
+
+
+class CreateReview(forms.ModelForm):
+    class Meta: 
+        model = Review
+        fields=('title', 'content',)
+        exclude=('publisher',)
