7fb8066c061d967c21806de8ac9782440dbbd78c
gyounghanha <gyounghan.ha@kt.com>
2020-07-31 18:12:54 +0900

    appointment create template 초안

--- blank_meeting/appointment/forms.py
+++ blank_meeting/appointment/forms.py
@@ -1,12 +1,21 @@
 from django import forms
 from .models import Appointment
-
+import datetime
 
 class CreateAppointment(forms.ModelForm):
     class Meta: 
+        renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
+
         model = Appointment
         fields=('title', 'content', 'place', 'time',)
         exclude=('publisher',)
+        
+    date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
+    def clean_date(self):
+        date = self.cleaned_data['date']
+        if date < datetime.date.today():
+           raise forms.ValidationError("The date cannot be in the past!")
+        return date
 
 class SearchForm(forms.ModelForm):
      word = forms.CharField(label='Search Word')
