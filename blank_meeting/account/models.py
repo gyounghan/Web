4170a1f3bbf8819047b8374fcc78dca70f5abc57
gyounghanha <gyounghan.ha@kt.com>
2020-08-11 15:48:20 +0900

    사용자 User model 확장 (Profile)
    - 기존은 Account, User model를 동시에 생성 및 관리
    - 수정 : User model 안에 profile model를 확장

--- blank_meeting/account/models.py
+++ blank_meeting/account/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
-class Account(models.Model) :
-    userid = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    is_active = models.BooleanField(default=False)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_score = models.IntegerField(null=True)
     matchmaker_score = models.IntegerField(null=True)
-    email_address = models.CharField(max_length=100)
+    email_address = models.CharField(max_length=100, default='temp')
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance).save()
